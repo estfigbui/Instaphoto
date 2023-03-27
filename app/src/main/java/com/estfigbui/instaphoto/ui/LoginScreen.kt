710906fa17b96e4a15dc16c713e666ae0678f3c3
estfigbui <estefaniafigbui@gmail.com>
2023-03-27 09:34:24 -0500

    Login/Sign Up basic user interface

--- app/src/main/java/com/estfigbui/instaphoto/ui/LoginScreen.kt
+++ app/src/main/java/com/estfigbui/instaphoto/ui/LoginScreen.kt
@@ -1,21 +1,74 @@
 package com.estfigbui.instaphoto.ui
 
 import android.app.Activity
+import android.util.Patterns
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.estfigbui.instaphoto.R
 
 @Composable
 fun MyLoginScreen() {
-    Box(Modifier.fillMaxSize()) {
+    Box(
+        Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
         MyHeader(Modifier.align(Alignment.TopEnd))
+        MyBody(Modifier.align(Alignment.Center))
+        MyFooter(Modifier.align(Alignment.BottomCenter))
+    }
+}
+
+@Composable
+fun MyFooter(modifier: Modifier) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .fillMaxWidth()
+        )
+        MySpacer(20)
+        MySignUp()
+        MySpacer(20)
+    }
+}
+
+@Composable
+fun MySignUp() {
+    Row(Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
+        Text(text = "Don´t have an account?", fontSize = 12.sp, color = Color(0xFFB5B5B5))
+        Text(
+            text = "Sign up.",
+            Modifier.padding(horizontal = 8.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFF4EA8E9)
+        )
     }
 }
 
@@ -28,3 +81,180 @@ fun MyHeader(modifier: Modifier) {
         modifier = modifier.clickable { activity.finish() }
     )
 }
+
+@Composable
+fun MyBody(modifier: Modifier) {
+    var email by rememberSaveable { mutableStateOf("") }
+    var password by rememberSaveable { mutableStateOf("") }
+    var isLoginEnabled by rememberSaveable { mutableStateOf(false) }
+
+    Column(modifier = modifier) {
+        MyImageLogo(Modifier.align(Alignment.CenterHorizontally))
+        MySpacer(16)
+        MyEmail(email) {
+            email = it
+            isLoginEnabled = enableLoginButton(email, password)
+        }
+        MySpacer(4)
+        MyPassword(password) {
+            password = it
+            isLoginEnabled = enableLoginButton(email, password)
+        }
+        MySpacer(8)
+        MyForgotPassword(Modifier.align(Alignment.End))
+        MySpacer(16)
+        MyLoginButton(isLoginEnabled)
+        MySpacer(16)
+        MyLoginDivider()
+        MySpacer(32)
+        MySocialLogin()
+    }
+}
+
+@Composable
+fun MySocialLogin() {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.facebook_icon),
+            contentDescription = "Facebook icon",
+            modifier = Modifier.size(16.dp)
+        )
+        Text(
+            text = "Continue as estfigbui",
+            fontSize = 14.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(horizontal = 8.dp),
+            color = Color(0xFF4EA8E9)
+        )
+    }
+}
+
+@Composable
+fun MyLoginDivider() {
+    Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .weight(1f)
+        )
+        Text(
+            text = "OR",
+            modifier = Modifier.padding(horizontal = 18.dp),
+            fontSize = 12.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color(0xFFB5B5B5)
+        )
+        Divider(
+            Modifier
+                .background(Color(0xFFF9F9F9))
+                .height(1.dp)
+                .weight(1f)
+        )
+    }
+}
+
+@Composable
+fun MyLoginButton(loginEnabled: Boolean) {
+    Button(
+        onClick = { },
+        enabled = loginEnabled,
+        modifier = Modifier.fillMaxWidth(),
+        colors = ButtonDefaults.buttonColors(
+            backgroundColor = Color(0xFF4EA8E9),
+            disabledBackgroundColor = Color(0xFF78C8F9),
+            contentColor = Color.White,
+            disabledContentColor = Color.White
+        )
+    ) {
+        Text(text = "Log In")
+    }
+}
+
+fun enableLoginButton(email: String, password: String) =
+    Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length > 6
+
+@Composable
+fun MyForgotPassword(modifier: Modifier) {
+    Text(
+        text = "Forgot password?",
+        fontSize = 12.sp,
+        fontWeight = FontWeight.Bold,
+        color = Color(0xFF4EA8E9),
+        modifier = modifier
+    )
+}
+
+@Composable
+fun MyPassword(password: String, onTextChanged: (String) -> Unit) {
+    var passwordVisibility by rememberSaveable { mutableStateOf(false) }
+    TextField(
+        value = password,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Password") },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            backgroundColor = Color(0xFFFAFAFA),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        trailingIcon = {
+            val image = if (passwordVisibility) {
+                Icons.Filled.VisibilityOff
+            } else {
+                Icons.Filled.Visibility
+            }
+            IconButton(onClick = { passwordVisibility = !passwordVisibility }) {
+                Icon(imageVector = image, contentDescription = "Show password")
+            }
+        },
+        visualTransformation = if (passwordVisibility) {
+            VisualTransformation.None
+        } else {
+            PasswordVisualTransformation()
+        }
+    )
+}
+
+@Composable
+fun MyEmail(email: String, onTextChanged: (String) -> Unit) {
+    TextField(
+        value = email,
+        onValueChange = { onTextChanged(it) },
+        modifier = Modifier.fillMaxWidth(),
+        placeholder = { Text(text = "Email") },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color(0xFFB2B2B2),
+            backgroundColor = Color(0xFFFAFAFA),
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        )
+    )
+}
+
+@Composable
+fun MySpacer(size: Int) {
+    Spacer(modifier = Modifier.size(size.dp))
+}
+
+@Composable
+fun MyImageLogo(modifier: Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.logo),
+        contentDescription = "Logo image",
+        modifier = modifier
+    )
+}
+
+
